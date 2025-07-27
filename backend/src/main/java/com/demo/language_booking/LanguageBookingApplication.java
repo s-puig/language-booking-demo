f7d337f62c85c4903a0b84bec79f4114bd08dca3
S. Puig <puig.sergi@outlook.com>
2025-07-27 19:27:25 +0200

    Initial Spring Initializr project

---
+++ backend/src/main/java/com/demo/language_booking/LanguageBookingApplication.java
@@ -0,0 +1,13 @@
+package com.demo.language_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageBookingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/demo/language_booking/LanguageBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.language_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

950da6f4121cbab55f4a6594e2855c5b9d01fe86
S. Puig <puig.sergi@outlook.com>
2025-08-03 19:44:18 +0200

    Add UserController and unit tests

---
+++ backend/src/main/java/com/demo/language_booking/users/UserController.java
@@ -0,0 +1,64 @@
+package com.demo.language_booking.users;
+
+
+import com.demo.language_booking.common.Country;
+import com.demo.language_booking.common.Language;
+import com.demo.language_booking.common.exceptions.ResourceNotFoundException;
+import com.demo.language_booking.users.dto.UserCreateRequest;
+import com.demo.language_booking.users.dto.UserPublicResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@RequestMapping(value = "/api/users", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    private UserMapper userMapper = UserMapper.USER_MAPPER;
+
+    @GetMapping
+    public ResponseEntity<List<UserPublicResponse>> getAll() {
+        List<User> users = userService.getAll();
+        List<UserPublicResponse> responses = users.stream()
+                .map(userMapper::mapToUserPublicResponse)
+                .toList();
+        return ResponseEntity.ok(responses);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserPublicResponse> create(@RequestBody @Valid UserCreateRequest userCreateRequest) {
+        User savedUser = userService.create(userCreateRequest);
+        UserPublicResponse response = userMapper.mapToUserPublicResponse(savedUser);
+        return ResponseEntity.created(
+                java.net.URI.create("/api/users/" + savedUser.getId())
+        ).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserPublicResponse> getById(@PathVariable Long id) {
+        User user = userService.getById(id).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+        UserPublicResponse response = userMapper.mapToUserPublicResponse(user);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserPublicResponse> update(@PathVariable Long id, @RequestBody @Valid UserCreateRequest userCreateRequest) {
+        User updatedUser = userService.update(id, userCreateRequest);
+        UserPublicResponse response = userMapper.mapToUserPublicResponse(updatedUser);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}

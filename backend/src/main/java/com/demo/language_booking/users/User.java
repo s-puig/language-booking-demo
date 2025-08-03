da9434a953b36e5485d3bb8414024aec09101e16
S. Puig <puig.sergi@outlook.com>
2025-08-03 20:25:48 +0200

    Fix default role

--- backend/src/main/java/com/demo/language_booking/users/User.java
+++ backend/src/main/java/com/demo/language_booking/users/User.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -40,8 +41,10 @@ public class User {
     @OneToMany(mappedBy = "user")
     private Set<UserLanguageLevel> spokenLanguages;
 
+    @Builder.Default
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
+    @ColumnDefault("'STUDENT'")
     private Role role = Role.STUDENT;
 
     @Column(name = "created_at", updatable = false)

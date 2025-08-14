12e2a06c54cfc55f49ddc4941b93cd5d4e99ccaa
S. Puig <puig.sergi@outlook.com>
2025-08-14 17:19:56 +0200

    Add Authenticated meta annotation to AuthorizeCompilation

--- backend/src/main/java/com/demo/language_booking/auth/authorization/AuthorizeCompilation.java
+++ backend/src/main/java/com/demo/language_booking/auth/authorization/AuthorizeCompilation.java
@@ -1,7 +1,10 @@
 package com.demo.language_booking.auth.authorization;
 
+import com.demo.language_booking.auth.authentication.Authenticated;
+
 import java.lang.annotation.*;
 
+@Authenticated
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited

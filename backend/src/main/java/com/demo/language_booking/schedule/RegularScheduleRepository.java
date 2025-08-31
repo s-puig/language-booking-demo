3ced2f443baf5bbda1e201e7207f26769157dc48
S. Puig <puig.sergi@outlook.com>
2025-08-31 19:52:13 +0200

    Fix Schedule JPARepository

--- backend/src/main/java/com/demo/language_booking/schedule/RegularScheduleRepository.java
+++ backend/src/main/java/com/demo/language_booking/schedule/RegularScheduleRepository.java
@@ -1,6 +1,10 @@
 package com.demo.language_booking.schedule;
 
+import com.demo.language_booking.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RegularScheduleRepository extends JpaRepository<RegularSchedule, Long> {
+
 }

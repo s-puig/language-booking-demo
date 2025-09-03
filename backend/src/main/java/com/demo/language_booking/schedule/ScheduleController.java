57909355389ddd21f12ef4edb8c6a0eacb6331f9
S. Puig <puig.sergi@outlook.com>
2025-09-03 18:25:40 +0200

    Add controller

--- backend/src/main/java/com/demo/language_booking/schedule/ScheduleController.java
+++ backend/src/main/java/com/demo/language_booking/schedule/ScheduleController.java
@@ -1,18 +1,21 @@
 package com.demo.language_booking.schedule;
 
 import com.demo.language_booking.auth.CurrentSession;
+import com.demo.language_booking.auth.authorization.AuthPolicy;
+import com.demo.language_booking.auth.authorization.Authorize;
+import com.demo.language_booking.common.exceptions.ResourceNotFoundException;
 import com.demo.language_booking.schedule.dto.ScheduleRequest;
 import com.demo.language_booking.schedule.dto.Schedule;
 import com.demo.language_booking.users.User;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
+@AllArgsConstructor
+@AuthPolicy(ScheduleSecurityPolicy.class)
 @RestController
 @RequestMapping("/api/schedules")
-@RequiredArgsConstructor
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
@@ -20,20 +23,28 @@ public class ScheduleController {
     private final ScheduleMapper scheduleMapper;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Schedule> getScheduleById(@PathVariable long id) {
-        Optional<RegularSchedule> schedule = scheduleService.findById(id);
-        return schedule.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Schedule> getById(@PathVariable long id) {
+        RegularSchedule schedule = scheduleService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Schedule resource does not exist"));
+        return ResponseEntity.ok(scheduleMapper.toSchedule(schedule));
     }
 
+    @Authorize(value = User.Role.TEACHER, requireOwnership = true)
     @PostMapping
-    public ResponseEntity<Schedule> createSchedule(CurrentSession user, @RequestBody ScheduleRequest request) {
+    public ResponseEntity<Schedule> create(CurrentSession user, @RequestBody ScheduleRequest request) {
         RegularSchedule createdSchedule = scheduleService.create(user.getId(), request);
         return ResponseEntity.ok(scheduleMapper.toSchedule(createdSchedule));
     }
 
+    @Authorize(User.Role.TEACHER)
+    @PutMapping("/{id}")
+    public ResponseEntity<Schedule> update(CurrentSession user, @RequestBody ScheduleRequest request) {
+        RegularSchedule schedule = scheduleService.update(user.getId(), request);
+        return ResponseEntity.ok(scheduleMapper.toSchedule(schedule));
+    }
+
+    @Authorize(value = User.Role.TEACHER, requireOwnership = true)
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSchedule(@PathVariable long id) {
+    public ResponseEntity<Void> delete(@PathVariable long id) {
         scheduleService.delete(id);
         return ResponseEntity.noContent().build();
     }

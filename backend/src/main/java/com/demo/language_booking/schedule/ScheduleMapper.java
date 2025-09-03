2d8cc2f7e7642cc89bc45a02908e26389c192e2b
S. Puig <puig.sergi@outlook.com>
2025-09-03 18:25:22 +0200

    Implement schedule mapper

--- backend/src/main/java/com/demo/language_booking/schedule/ScheduleMapper.java
+++ backend/src/main/java/com/demo/language_booking/schedule/ScheduleMapper.java
@@ -3,6 +3,8 @@ package com.demo.language_booking.schedule;
 import com.demo.language_booking.schedule.dto.Schedule;
 import com.demo.language_booking.schedule.dto.ScheduleRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -18,21 +20,23 @@ public interface ScheduleMapper {
     ScheduleMapper SCHEDULE_MAPPER = Mappers.getMapper(ScheduleMapper.class);
 
     RegularSchedule toRegularSchedule(ScheduleRequest schedule);
+    @Mapping(source = "id", target = "tutor_id")
     Schedule toSchedule(RegularSchedule schedule);
 
     default BitSet map(boolean[][] weekHours) {
         BitSet bitSet = new BitSet(7*24);
 
         IntStream.range(0, 7 * 24)
-                .filter(i -> weekHours[i / 24][i % 24]) // true slots only
+                .filter(i -> weekHours[i / 24][i % 24])
                 .forEach(bitSet::set);
 
         return bitSet;
     }
 
-    default byte[] map(BitSet bitSet) {
-        if (bitSet == null) throw new IllegalArgumentException("availableHours cannot be null");
-        return bitSet.toByteArray();
+    default boolean[][] map(BitSet bitSet) {
+        boolean[][] weekHours = new boolean[7][24];
+        bitSet.stream().forEach(idx -> weekHours[idx / 24][idx % 24] = true);
+        return weekHours;
     }
 
     default ZoneId map(String timezone) {

8a56f8fe285c4640014ac8bce7e12fa7813f540b
S. Puig <puig.sergi@outlook.com>
2025-10-10 13:08:01 +0200

    Add provisional Lesson entity

---
+++ backend/src/main/java/com/demo/language_booking/lessons/Lesson.java
@@ -0,0 +1,38 @@
+package com.demo.language_booking.lessons;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "lessons")
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "lesson_id")
+    private long id;
+
+    @Column(name = "lesson_name")
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "price", precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
